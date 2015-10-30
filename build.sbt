0d5e88b8c9cb8b84d1514d35df16fc9b1f7d08d7
Chandu <cchhaanndduu@gmail.com>
2015-10-30 02:53:33 -0400

    Add sbt build file, and a startup scala file

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "pomodoro-timer"
+
+version := "0.0.1"
+
+scalaVersion := "2.11.7"
---
+++ src/main/scala/com/chandu/pomodoro/app/PomodoroTimer.scala
@@ -0,0 +1,9 @@
+package com.chandu.pomodoro.app
+
+import scala.swing._
+
+object PomodoroTimer extends SwingApplication {
+  override def startup(args: Array[String]) {
+    println(true)
+  }
+}
